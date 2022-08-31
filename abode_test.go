7629489c9dfa23b8f804594a32bfba8c0dd24e72
Matty Evans <mattevansnz@gmail.com>
2022-09-01 09:44:46 +1000

    feat(timezone): added interface to fetch Timezone info via gmaps api

--- abode_test.go
+++ abode_test.go
@@ -1,6 +1,7 @@
 package abode
 
 import (
+	"context"
 	"reflect"
 	"testing"
 
@@ -8,92 +9,146 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestExplodeUSAddress(t *testing.T) {
-	// Register the test.
+func TestExplode_US(t *testing.T) {
 	RegisterTestingT(t)
 
-	// Init client
 	if client == nil {
-		err := initClient()
-		if err != nil {
+		if err := mapsClient(); err != nil {
 			t.Errorf("Unexpected error: %s", err)
 		}
 	}
 
-	line1 := "193 Rogers Avenue"
-	line2 := "Brooklyn"
-	state := "New York"
-	country := "United States"
-	countryCode := "US"
-	zip := "11216"
-	lat := 40.6706252
-	lng := -73.9530545
-	formatted := "193 Rogers Ave, Brooklyn, NY 11216, USA"
+	var (
+		line1       = "193 Rogers Avenue"
+		line2       = "Brooklyn"
+		state       = "New York"
+		country     = "United States"
+		countryCode = "US"
+		zip         = "11216"
+		lat         = 40.6706252
+		lng         = -73.9530545
+		formatted   = "193 Rogers Ave, Brooklyn, NY 11216, USA"
 
-	expected := &Address{
-		AddressLine1:     &line1,
-		AddressLine2:     &line2,
-		AddressCity:      nil,
-		AddressState:     &state,
-		AddressCountry:   &country,
-		AddressCountryCode: &countryCode,
-		AddressZip:       &zip,
-		AddressLat:       &lat,
-		AddressLng:       &lng,
-		FormattedAddress: &formatted,
-	}
+		expected = &Address{
+			AddressLine1:       &line1,
+			AddressLine2:       &line2,
+			AddressCity:        nil,
+			AddressState:       &state,
+			AddressCountry:     &country,
+			AddressCountryCode: &countryCode,
+			AddressZip:         &zip,
+			AddressLat:         &lat,
+			AddressLng:         &lng,
+			FormattedAddress:   &formatted,
+		}
+	)
 
-	address1 := "193 Rogers Ave, Brooklyn, New York"
-	result, err := Explode(address1)
+	result, err := ExplodeWithContext(context.Background(), "193 Rogers Ave, Brooklyn, New York")
 	if err != nil {
 		t.Errorf("Unexpected error: %s", err)
 	}
+
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("Unexpected Results\n%s", pretty.Compare(result, expected))
 	}
 }
 
-func TestExplodeInternationalAddress(t *testing.T) {
-	// Register the test.
+func TestExplode_International(t *testing.T) {
 	RegisterTestingT(t)
 
-	// Init client
 	if client == nil {
-		err := initClient()
-		if err != nil {
+		if err := mapsClient(); err != nil {
 			t.Errorf("Unexpected error: %s", err)
 		}
 	}
 
-	line1 := "1 4 Abercrombie Street"
-	line2 := "Howick"
-	city := "Auckland"
-	state := "Auckland"
-	country := "New Zealand"
-	countryCode := "NZ"
-	zip := "2014"
-	lat := -36.8990751
-	lng := 174.9334851
-	formatted := "1/4 Abercrombie Street, Howick, Auckland 2014, New Zealand"
+	var (
+		line1       = "1 4 Abercrombie Street"
+		line2       = "Howick"
+		city        = "Auckland"
+		state       = "Auckland"
+		country     = "New Zealand"
+		countryCode = "NZ"
+		zip         = "2014"
+		lat         = -36.8990751
+		lng         = 174.9334851
+		formatted   = "1/4 Abercrombie Street, Howick, Auckland 2014, New Zealand"
 
-	expected := &Address{
-		AddressLine1:     &line1,
-		AddressLine2:     &line2,
-		AddressCity:      &city,
-		AddressState:     &state,
-		AddressCountry:   &country,
-		AddressCountryCode: &countryCode,
-		AddressZip:       &zip,
-		AddressLat:       &lat,
-		AddressLng:       &lng,
-		FormattedAddress: &formatted,
-	}
+		expected = &Address{
+			AddressLine1:       &line1,
+			AddressLine2:       &line2,
+			AddressCity:        &city,
+			AddressState:       &state,
+			AddressCountry:     &country,
+			AddressCountryCode: &countryCode,
+			AddressZip:         &zip,
+			AddressLat:         &lat,
+			AddressLng:         &lng,
+			FormattedAddress:   &formatted,
+		}
+	)
 
-	address1 := "1/4 Abercrombie Street, Auckland"
-	result, err := Explode(address1)
+	result, err := ExplodeWithContext(context.Background(), "1/4 Abercrombie Street, Auckland")
 	if err != nil {
 		t.Errorf("Unexpected error: %s", err)
 	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Unexpected Results\n%s", pretty.Compare(result, expected))
+	}
+}
+
+func TestTimezone_US(t *testing.T) {
+	RegisterTestingT(t)
+
+	if client == nil {
+		if err := mapsClient(); err != nil {
+			t.Errorf("Unexpected error: %s", err)
+		}
+	}
+
+	var (
+		expected = &Location{
+			DstOffset:    0,
+			RawOffset:    -17762,
+			TimeZoneId:   "America/New_York",
+			TimeZoneName: "GMT-04:56:02",
+		}
+	)
+
+	result, err := Timezone(context.Background(), "193 Rogers Ave, Brooklyn, New York")
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Unexpected Results\n%s", pretty.Compare(result, expected))
+	}
+}
+
+func TestTimezone_International(t *testing.T) {
+	RegisterTestingT(t)
+
+	if client == nil {
+		if err := mapsClient(); err != nil {
+			t.Errorf("Unexpected error: %s", err)
+		}
+	}
+
+	var (
+		expected = &Location{
+			DstOffset:    0,
+			RawOffset:    41944,
+			TimeZoneId:   "Pacific/Auckland",
+			TimeZoneName: "GMT+11:39:04",
+		}
+	)
+
+	result, err := Timezone(context.Background(), "1/4 Abercrombie Street, Auckland")
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("Unexpected Results\n%s", pretty.Compare(result, expected))
 	}
